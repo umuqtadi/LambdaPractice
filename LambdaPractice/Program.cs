21a1680df761bab7c5e1324e74c317e71d4ecac3
Umar Muqtadir <umar.muqtadir@gmail.com>
2015-04-20 22:12:15 -0600

    Completed

--- LambdaPractice/Program.cs
+++ LambdaPractice/Program.cs
@@ -42,60 +42,89 @@ namespace LambdaPractice
             }
 
             //sort ballProducts alphabetically and print them to the console.
+            foreach (string ballStuff in ballProducts.OrderBy(x=> x))
+            {
+                Console.WriteLine(ballStuff);
+            }
 
             //print the product with the longest name to the console using the .First() extension.
+            string longest = products.OrderByDescending(x => x.Length).First();
+            Console.WriteLine(longest);
 
             //print the product with the shortest name to the console using the .First() extension.
+            string shortest = products.OrderBy(x => x.Length).First();
+            Console.WriteLine(shortest);
 
             //print the product with the 3rd shortest name to the console using an index or Skip/Take (you must convert the results to a list using .ToList()).  
+            string thirShort = products.OrderBy(x => x.ToList().Count).ElementAt(2);
+            Console.WriteLine(thirShort);
 
             //print the ballProduct with the 2nd longest name to the console using an index or Skip/Take (you must convert the results to a list using .ToList()). 
+            string secLong = products.OrderByDescending(x => x.ToList().Count).ElementAt(1);
+            Console.WriteLine(secLong);
 
             //declare a variable reversedProducts and set it equal to all products ordered by the longest word first. (use the OrderByDescending() extension).
+            string reversedProducts = products.OrderByDescending(x => x.Length).First();
+            Console.WriteLine(reversedProducts);
 
             //print out the reversedProducts to the console using a foreach loop.
+            foreach (char reverSa in reversedProducts)
+            {
+                Console.WriteLine(reverSa);
+            }
 
             //print out all the products ordered by the longest word first using the OrderByDecending() extension and a foreach loop.  
             //Note: you will not use a variable to store your list
+            foreach (string theList in products.OrderByDescending(x => x.Length))
+            {
+                Console.WriteLine(theList);
+            }
+
+
 
             //FILL IN THE FUNCTIONS BELOW TO MAKE THE TESTS PASS
-
             Console.ReadKey();
         }
 
         public static string LongestName(List<string> inputList)
         {
+            string varIn = inputList.OrderByDescending(x => x.Length).First();
             //with the input list, return the item with the longest name
-            return string.Empty;
+            return varIn;
         }
 
         public static string ShortestName(List<string> inputList)
         {
+            string shorMan = inputList.OrderBy(x => x.Length).First();
             //with the input list, return the item with the shortest name
-            return string.Empty;
+            return shorMan;
         }
 
         public static string SecondLongestName(List<string> inputList)
         {
+            string twoTone = inputList.OrderByDescending(x => x.ToList().Count).ElementAt(1);
             //with the input list, return the item with the second longest name
-            return string.Empty;
+            return twoTone;
         }
 
         public static string ThirdShortestName(List<string> inputList)
         {
+            string thirtyGlock = inputList.OrderBy(x => x.ToList().Count).ElementAt(2);
             //with the input list, return the item with the third shortest name
-            return string.Empty;
+            return thirtyGlock;
         }
 
         public static List<string> BallProducts(List<string> inputList)
         {
+            List<string> itHasIt = inputList.Where(x => x.Contains("ball")).ToList();
             //with the input list, return a list with only the the products that contain the word ball
-            return inputList;
+            return itHasIt;
         }
         public static List<string> EndInS(List<string> inputList)
         {
+            List<string> endIt = inputList.Where(x => x.EndsWith("s")).ToList();
             //with the input list, return a list with only the the products that end with the letter s
-            return inputList;
+            return endIt;
         }
     }
 }
